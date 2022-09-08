710ea794937cec7f923f0127d46f384e4936dac2
zippomen111 <zippomen111@gmail.com>
2022-09-08 12:18:31 +0100

    sort+category moved to redux store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route } from "react-router-dom"
+
+import "./App.scss"
+import { Header } from "./components/Header/Header"
+import { Home } from './pages/Home';
+import { Cart } from './pages/Cart';
+import { NotFound } from './pages/NotFound';
+import { Footer } from './components/Footer/Footer';
 
 function App() {
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <main className="content">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 }
